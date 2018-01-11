2b50f9869d7e2f023e1011c9e86a29be2df1ada8
Alexandru Aldulea <alex@Alexandrus-MBP.fritz.box>
2018-01-11 22:48:37 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Game from './Game';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById('root'));
 registerServiceWorker();
